28c167c8ac4591ae404f063162950116bd77d4d3
Kalyan Adithya <kalyanadithya12@gmail.com>
2019-03-30 18:00:14 +0530

    Issue #282 fixed

--- src/lab/exp2/quizconfig.js
+++ src/lab/exp2/quizconfig.js
@@ -6,69 +6,73 @@
 ***********************************************/
 
 //Enter total number of questions:
-var totalquestions=7
+var totalquestions = 7
 
 //Enter the solutions corresponding to each question:
-var correctchoices=new Array()
-correctchoices[1]='a' //question 1 solution
-correctchoices[2]='a' //question 2 solution, and so on.
-correctchoices[3]='a'
-correctchoices[4]='a'
-correctchoices[5]='d'
-correctchoices[6]='a'
-correctchoices[7]='a'
+var correctchoices = new Array()
+correctchoices[1] = 'a' //question 1 solution
+correctchoices[2] = 'a' //question 2 solution, and so on.
+correctchoices[3] = 'a'
+correctchoices[4] = 'a'
+correctchoices[5] = 'd'
+correctchoices[6] = 'a'
+correctchoices[7] = 'a'
 
 
 /////Don't edit beyond here//////////////////////////
 
-function gradeit(){
-var incorrect=null
-for (q=1;q<=totalquestions;q++){
-	var thequestion=eval("document.myquiz.question"+q)
-	for (c=0;c<thequestion.length;c++){
-		if (thequestion[c].checked==true)
-		actualchoices[q]=thequestion[c].value
+function gradeit() {
+	var incorrect = null
+	for (q = 1; q <= totalquestions; q++) {
+		var thequestion = eval("document.myquiz.question" + q)
+		for (c = 0; c < thequestion.length; c++) {
+			if (thequestion[c].checked == true)
+				actualchoices[q] = thequestion[c].value
 		}
-		
-	if (actualchoices[q]!=correctchoices[q]){ //process an incorrect choice
-		if (incorrect==null)
-		incorrect=q
-		else
-		incorrect+="/"+q
+
+		if (actualchoices[q] != correctchoices[q]) { //process an incorrect choice
+			if (incorrect == null)
+				incorrect = q
+			else
+				incorrect += "/" + q
 		}
 	}
 
-if (incorrect==null)
-incorrect="a/b"
-document.cookie='q='+incorrect
-if (document.cookie=='')
-alert("Your browser does not accept cookies. Please adjust your browser settings.")
-else
-window.location="results.htm"
+	if (incorrect == null)
+		incorrect = "a/b"
+	document.cookie = 'q=' + incorrect
+	if (document.cookie == '')
+		alert("Your browser does not accept cookies. Please adjust your browser settings.")
+	else
+		window.location = "results.htm"
 }
 
 
-function showsolution(){
-var win2=window.open("","win2","width=200,height=350, scrollbars")
-win2.focus()
-win2.document.open()
-win2.document.write('<title>Solution</title>')
-win2.document.write('<body bgcolor="#FFFFFF">')
-win2.document.write('<center><h3>Solution to Quiz</h3></center>')
-win2.document.write('<center><font face="Arial">')
-for (i=1;i<=totalquestions;i++){
-for (temp=0;temp<incorrect.length;temp++){
-if (i==incorrect[temp])
-wrong=1
+window.onload = function () {
+	document.getElementById("showsolution").hidden = true;
 }
-if (wrong==1){
-win2.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>")
-wrong=0
-}
-else
-win2.document.write("Question "+i+"="+correctchoices[i]+"<br>")
-}
-win2.document.write('</center></font>')
-win2.document.write("<h5>Note: The solutions in red are the ones to the questions you had incorrectly answered.</h5>")
-win2.document.close()
+function showsolution() {
+	document.getElementById("solutionTableBody").innerHTML = "";
+	this.disabled = true
+	document.getElementById("showsolution").hidden = false;
+	for (var i = totalquestions; i >= 1; i--) {
+		var colo = "green"
+		var ans = this.correctchoices[i]
+		for (temp = 0; temp < incorrect.length; temp++) {
+			if (i == incorrect[temp])
+				wrong = 1
+		}
+		if (wrong == 1) {
+			colo = "red"
+			wrong = 0
+		}
+		var str1 = ans
+		var str2 = str1.fontcolor(colo);
+		let table = document.getElementById("solutionTableBody")
+		let row = table.insertRow(0);
+		let column1 = row.insertCell(0);
+		let column2 = row.insertCell(1);
+		column1.innerHTML = i;
+		column2.innerHTML = str2;
+	}
 }
