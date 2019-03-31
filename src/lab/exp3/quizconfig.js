94f77eb16a2bf3a2c988f9cfd5da2d2a1f033370
maheshmylavarapu0057 <mylavarapu.maheswara@students.iiit.ac.in>
2019-03-31 19:20:42 +0530

    #206

--- src/lab/exp3/quizconfig.js
+++ src/lab/exp3/quizconfig.js
@@ -21,26 +21,39 @@ correctchoices[7]='b'
 
 /////Don't edit beyond here//////////////////////////
 
-function gradeit(){
+function gradeit()
+{
 var incorrect=null
-for (q=1;q<=totalquestions;q++){
+var cnt=0
+for (q=1;q<=totalquestions;q++)
+            {
+	actualchoices[q]='e'
 	var thequestion=eval("document.myquiz.question"+q)
-	for (c=0;c<thequestion.length;c++){
+	for (c=0;c<thequestion.length;c++)
+                 {
 		if (thequestion[c].checked==true)
 		actualchoices[q]=thequestion[c].value
 		}
-		
-	if (actualchoices[q]!=correctchoices[q]){ //process an incorrect choice
+		  if(actualchoices[q]!='e')
+                {
+	if (actualchoices[q]!=correctchoices[q])
+             { 
+                   //process an incorrect choice
 		if (incorrect==null)
 		incorrect=q
 		else
 		incorrect+="/"+q
 		}
 	}
+	 else
+        cnt++
+
+	}
 
 if (incorrect==null)
 incorrect="a/b"
 document.cookie='q='+incorrect
+document.cookie='got='+cnt
 if (document.cookie=='')
 alert("Your browser does not accept cookies. Please adjust your browser settings.")
 else
@@ -48,7 +61,8 @@ window.location="results.htm"
 }
 
 
-function showsolution(){
+function showsolution()
+ {
 var win2=window.open("","win2","width=200,height=350, scrollbars")
 win2.focus()
 win2.document.open()
@@ -56,12 +70,15 @@ win2.document.write('<title>Solution</title>')
 win2.document.write('<body bgcolor="#FFFFFF">')
 win2.document.write('<center><h3>Solution to Quiz</h3></center>')
 win2.document.write('<center><font face="Arial">')
-for (i=1;i<=totalquestions;i++){
-for (temp=0;temp<incorrect.length;temp++){
+for (i=1;i<=totalquestions;i++)
+{
+for (temp=0;temp<incorrect.length;temp++)
+ {
 if (i==incorrect[temp])
 wrong=1
 }
-if (wrong==1){
+if (wrong==1)
+ {
 win2.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>")
 wrong=0
 }
@@ -72,3 +89,7 @@ win2.document.write('</center></font>')
 win2.document.write("<h5>Note: The solutions in red are the ones to the questions you had incorrectly answered.</h5>")
 win2.document.close()
 }
+
+    
+
+
