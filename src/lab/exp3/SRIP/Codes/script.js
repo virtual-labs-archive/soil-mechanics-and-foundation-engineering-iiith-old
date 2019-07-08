803e40fddb7dd8cbb26b911bb0300e74e4532f2e
aditya <adityarathi42@gmail.com>
2019-07-08 18:24:31 +0530

    animation added

--- src/lab/exp3/SRIP/Codes/script.js
+++ src/lab/exp3/SRIP/Codes/script.js
@@ -6,9 +6,10 @@ var draw = SVG("animation").size(1300, 500)
 function myFunction(x) {
   if (x.matches) { // If media query matches
 	  draw.clear();
+
+	 
 	  alert("Please view the experiment in full desktop browser window!")
 	  
-
  
   } else {
 	  
@@ -45,16 +46,18 @@ function myFunction(x) {
 	  
  // var beaker1 = draw.polygon([[160,275],[160,125],[230,125],[230,275]]).fill('none').stroke({ width: 2});
 	  
-	  var beaker1 = draw.rect(70,150).radius(40,20).attr({
+	  var beaker1 = draw.rect(70,150).attr({
 		  x: 160,
+		  y: 125,
 		  y: 125,
 		  'stroke': 'black',
 		  'stroke-width': 2.5,
 		  'fill': 'none',
+		 
 		  
 	  });
 	  
-	   var beaker2 = draw.rect(70,150).radius(40,20).attr({
+	   var beaker2 = draw.rect(70,150).attr({
 		  x: 460,
 		  y: 125,
 		  'stroke': 'black',
@@ -63,7 +66,7 @@ function myFunction(x) {
 		  
 	  });
 	  
-	   var beaker3 = draw.rect(70,150).radius(40,20).attr({
+	   var beaker3 = draw.rect(70,150).attr({
 		  x: 760,
 		  y: 125,
 		  'stroke': 'black',
@@ -73,7 +76,7 @@ function myFunction(x) {
 	  });
 	  
 	  
-	   var beaker4 = draw.rect(70,150).radius(40,20).attr({
+	   var beaker4 = draw.rect(70,150).attr({
 		  x: 1060,
 		  y: 125,
 		  'stroke': 'black',
@@ -81,9 +84,56 @@ function myFunction(x) {
 		  'fill': 'none',
 		  
 	  });
-	
+
  
   }
+}
+
+function toggleAnimation() {
+	
+	$('#beaker2')
+    .animate({
+      'height': 0
+    },0)
+    .animate({
+      'height': 48,
+      'top': 333
+    }, 2000)
+	
+	$('#beaker3soil')
+    .animate({
+      'height': 0
+    }, 2000)
+    .animate({
+      'height': 48,
+      'top': 333
+    }, 0)
+
+	$('#beaker3water')
+    .animate({
+      'height': 0
+    }, 2000)
+    .animate({
+      'height': 101,
+      'top': 233
+    }, 2500)
+
+				
+	
+	
+	$('#beaker4')
+    .animate({
+      'height': 0
+    }, 4500)
+    .animate({
+      'height': 148,
+      'top': 233
+    }, 3200)
+	
+	startButton.onclick = function () {
+		alert("The simulation is already running!")
+	}
+
 }
 
 var x = window.matchMedia("(max-width: 1100px)")
@@ -92,50 +142,81 @@ x.addListener(myFunction) // Attach listener function on state changes
 
 
 
+var startButton = document.getElementById("start");
+var resetButton = document.getElementById("reset");
+
+
+startButton.onclick = function () {
+
+  toggleAnimation();
+
+  setTimeout(function () {
+    alert('Simulation Completed!');
+    location.reload();
+  }, 7900)
+
+
+}
+
+resetButton.onclick = function () {
+  location.reload();
+}
+
+var calcButton = document.getElementById("calc-button");
+
+calcButton.onclick = function () {
+  document.getElementById("myForm").scrollIntoView({
+    behavior: 'smooth',
+    block: 'center'
+  });
+}
+
+
 function toggle() {
 
 
   // HORIZONTAL WATER ANIMATIONS USING SVG.JS
 
   var incomingPipeHorizontal = DRAW.rect(1, 18).attr({
-    'fill': '#00B0EA',
-    x: 151,
+    'fill': '#00B0EA', x: 151,
     y: 151
+	  
   });
 
   incomingPipeHorizontal.animate().size(148, 18);
 
   var incomingPipeVertical = DRAW.rect(18, 0.01).attr({
-    'fill': '#00B0EA',
-    x: 281,
-    y: 168
+    'fill': '#00B0EA', x: 281,y: 168
   });
 
+	
+	
+	
+	
   incomingPipeVertical.animate({
-    delay: '1s'
+    
+	  delay: '1s'
   }).size(18, 280);
 
   var ductRect = DRAW.rect(0.01, 23).attr({
-    x: 401,
-    y: 381,
+    x: 401,y: 381,
     'fill': '#00B0EA'
   });
 
   ductRect.animate(4600, '', 2500).size(307, 23)
 
   ductTriangle1 = DRAW.polygon([
-    [401, 404],
-    [401, 433],
-    [401.1, 401]
+    [401, 404],[401, 433], [401.1, 401]
   ]).attr({
     'fill': '#00B0EA'
   });
 
   ductTriangle1.animate(1000, '', 2500).size(70, 21.5);
 
+	
+	
   ductTriangle2 = DRAW.polygon([
-    [650.5, 401],
-    [652.1, 400],
+    [650.5, 401],[652.1, 400],
     [652.1, 406.5]
   ]).attr({
     'fill': '#00B0EA'
@@ -156,11 +237,11 @@ function toggle() {
 
   var outgoingPipeVertical = DRAW.rect(18, 0.01).attr({
     'fill': '#00B0EA',
-    x: 981,
-    y: 409
+    x: 981,  y: 409
   });
 
-  outgoingPipeVertical.animate({
-    delay: '8.6s'
+  outgoingPipeVertical.animate({delay: '8.6s'
   }).size(18, 139);
 }
+
+
